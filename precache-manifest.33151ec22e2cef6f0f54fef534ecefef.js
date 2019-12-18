145d01094c86e60065688a0cddbc1c6cf9578455
Bhavatarini Kumaravel <bava@xm.studio>
2019-12-18 23:23:33 +0530

    Updates

---
+++ precache-manifest.33151ec22e2cef6f0f54fef534ecefef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c9a2c48fc62fa7f43341d55abf7c84",
+    "url": "/xm-studio-website/index.html"
+  },
+  {
+    "revision": "f416c6b3d8611a3bb773",
+    "url": "/xm-studio-website/static/css/main.c9bac252.chunk.css"
+  },
+  {
+    "revision": "aa1a30ff4c2b26b22f62",
+    "url": "/xm-studio-website/static/js/2.57d84820.chunk.js"
+  },
+  {
+    "revision": "f416c6b3d8611a3bb773",
+    "url": "/xm-studio-website/static/js/main.1cb6ab78.chunk.js"
+  },
+  {
+    "revision": "99cd78451598834ad761",
+    "url": "/xm-studio-website/static/js/runtime-main.ea593941.js"
+  }
+]);
